ef1d11fff88c87ec2d5f41ba7ae0ecc0701d26e4
emoilanen <moieli@hotmail.fi>
2022-12-27 14:23:27 +0200

    Install node modules and start scripts from root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useEffect, useState } from 'react';
 import Container from './components/layout/Container';
 import BookTable from './components/bookTable/BookTable';
--- frontend/src/components/bookForm/FormContainer.tsx
+++ frontend/src/components/bookForm/FormContainer.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import React from 'react';
 import { BREAKPOINT } from "../../theme";
 
 export const FormContainer = styled.div`
